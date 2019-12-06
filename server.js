1c6d96e0cb897360fcccf88bf75588c02a1b12a2
Ibrahima MBENGUE <mbenguei@ept.sn>
2019-12-06 18:24:47 +0000

    access permission for post desabled

--- server.js
+++ server.js
@@ -2,9 +2,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const dbConfig = require('./config/database.config.js');
 const mongoose = require('mongoose');
+var cors = require('cors');
+
+
 // create express app
 const app = express();
 const port = process.env.PORT || 3000;
+// ask permission
+app.use(cors());
+// Access-Control-Allow-Origin: *
+// Access-Control-Allow-Methods: POST,GET,PUT,DELETE
+// Access-Control-Allow-Headers: Authorization, Lang
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 // Configuring the database
