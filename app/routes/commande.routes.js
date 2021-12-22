67551c70829a6dde38131913670aea98913d0595
Ibrahima MBENGUE <imbengue027@gmail.com>
2021-12-22 13:38:26 +0000

    initial commit

---
+++ app/routes/commande.routes.js
@@ -0,0 +1,18 @@
+module.exports = (app) => {
+    const students = require('../controllers/commande.controller.js');
+
+    // Create a new commande
+    app.post('/commandes', commandes.create);
+
+    // Retrieve all commandes
+    app.get('/commandes', commandes.findAll);
+
+    // Retrieve a single commande with commandeId
+    app.get('/commandes/:commandeId', commandes.findOne);
+
+    // Update a commande with commandeId
+    app.put('/commandes/:commandeId', commandes.update);
+
+    // Delete a commande with commandeId
+    app.delete('/commandes/:commandeId', commandes.delete);
+}
